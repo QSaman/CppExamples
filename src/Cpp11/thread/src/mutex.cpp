480e04136eeea01cf176214b57f97a80f617490b
Saman Saadi <QSaman@users.noreply.github.com>
2017-10-18 13:20:23 -0400

    Add lock_guard example.

--- src/Cpp11/thread/src/mutex.cpp
+++ src/Cpp11/thread/src/mutex.cpp
@@ -28,6 +28,15 @@ void h(int id)
     cout << "I am " << id << " and I'm not in critical section" << endl;
 }
 
+void h1(int id)
+{
+    {
+        lock_guard<mutex> lock{m};
+        cout << "I am " << id << " and I'm in critical section (lock_guard)" << endl;
+    }
+    cout << "I am " << id << " and I'm not in critical section" << endl;
+}
+
 int main()
 {
     thread t1{f, 1};
@@ -42,6 +51,10 @@ int main()
     thread t6{h, 6};
     t5.join();
     t6.join();
+    thread t7{h1, 7};
+    thread t8{h1, 8};
+    t7.join();
+    t8.join();
     std::mutex m_test;
     m_test.lock();
     //If you uncomment the following line, it's a deadlock! In other words you cannot see "Hello World!"
