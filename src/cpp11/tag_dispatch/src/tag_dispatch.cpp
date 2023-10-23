f6776a17173cb4d560a5e847990698f7875a9395
Saman Saadi <QSaman@users.noreply.github.com>
2023-10-22 20:03:44 -0400

    Improved tag dispatch

--- src/cpp11/tag_dispatch/src/tag_dispatch.cpp
+++ src/cpp11/tag_dispatch/src/tag_dispatch.cpp
@@ -5,18 +5,24 @@
 #include <cassert>
 #include <cmath>
 #include <iostream>
+#include <string>
 #include <type_traits>
 
 namespace implementation_details
 {
+template <typename T, bool B>
+struct Tag
+{
+};
+
 template <typename Integer>
-bool areEqual(Integer i1, Integer i2, std::true_type)
+bool areEqual(Integer i1, Integer i2, Tag<std::is_integral<Integer>, true>)
 {
 	return i1 == i2;
 }
 
 template <typename Floating>
-bool areEqual(Floating f1, Floating f2, std::false_type)
+bool areEqual(Floating f1, Floating f2, Tag<std::is_floating_point<Floating>, true>)
 {
 	return fabs(f1 - f2) <= 1e-2;
 }
@@ -25,8 +31,14 @@ bool areEqual(Floating f1, Floating f2, std::false_type)
 template <typename Number>
 bool areEqual(Number n1, Number n2)
 {
-	constexpr std::is_integral<Number> is_Integral;
-	return implementation_details::areEqual(n1, n2, is_Integral);
+	using implementation_details::Tag;
+
+	using FinalTag = typename std::conditional<std::is_integral<Number>::value,
+		Tag<std::is_integral<Number>, std::is_integral<Number>::value>,
+	Tag<std::is_floating_point<Number>, std::is_floating_point<Number>::value>>::type;
+
+	FinalTag tag;
+	return implementation_details::areEqual(n1, n2, tag);
 }
 
 int main()
@@ -36,4 +48,7 @@ int main()
 
 	assert(!areEqual(1, 2));
 	assert(!areEqual(1.1, 1.2));
+
+	// compile error:
+	// areEqual(std::string("1"), std::string("1")); 
 }
